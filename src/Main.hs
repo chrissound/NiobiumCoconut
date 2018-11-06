04080f932f3d02ce2f8c90a57c412a562af43fa0
Chris Stryczynski <chris@soundshare.co.uk>
2018-11-06 13:42:28 +0000

    Woo fixed form

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE OverloadedStrings #-}
+--{-# LANGUAGE ScopedTypeVariables #-}
+--{-# LANGUAGE MultiParamTypeClasses #-}
+--{-# LANGUAGE FlexibleInstances #-}
+--{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
-import Data.Text
+import Data.Text (Text)
 -- import Yesod.Form.Types
 import Control.Monad
+import Control.Applicative
+--import Data.Proxy
 
 main :: IO ()
 main = renderNioForm testForm
@@ -20,7 +26,8 @@ data NioForm = NioForm {
   fields :: [NioFieldView]
   }
 
-data NioFieldError = NioFieldError Text
+data NioFieldError = NioFieldErrorEmty Text
+
 data NioFieldInput =
     NioFieldInputHidden
   | NioFieldInputText
@@ -40,23 +47,57 @@ instance Show NioFieldView where
   show NioFieldView { fvValue = x, fvType = y} = do
     mconcat [show x, " ", show y]
 
+class FieldGetter a where
+  getField' :: String ->  a
+
+instance FieldGetter Bool where
+  getField' _ = True
+
+instance FieldGetter Text where
+  getField' _ = ""
+
+instance FieldGetter Int where
+  getField' _ = 123
+
 renderNioForm :: NioForm -> IO ()
 renderNioForm nf = forM_ (fields nf) $ \x -> do
   print x
 
 testForm :: NioForm
 testForm = NioForm [
-    NioFieldView "Test" "test" [] NioFieldInputText "test"
+    NioFieldView "Test" "test" []   NioFieldInputText "test"
   , NioFieldView "Test2" "test2" [] NioFieldInputText "test"
   , NioFieldView "Test3" "test3" []
     (NioFieldInputMultiple [("a","a"), ("b","b")])
     "test"
-  , NioFieldView "Test4" "test4" [] NioFieldInputDigit "0"
+  , NioFieldView "Test4" "test4" [] NioFieldInputDigit 5
   ]
 
-data TestForm = TestForm Text Text Bool
+data TestForm = TestForm Text Text Bool Int
+data TestForm2 = TestForm2 Text
+data TestFormFields =
+    TestFormField1
+  | TestFormField2
+  | TestFormFieldBool3
+  | TestFormFieldInt4
 
--- inputTest = TestForm
---   <$> getField "Test"
---   <*> getField "Test2"
---   <*> getField "Test3"
+data MyEither a b = MyLeft a | MyRight b
+
+inputTest :: [String] -> Either Int TestForm
+inputTest = (liftM4 TestForm)
+  <$> (getField ((==) "test") testForm "Test")
+  <*> (getField (const True) testForm "Test2")
+  <*> (getField (const True) testForm "Test3")
+  <*> (getField ((==) 5) testForm "Test4")
+
+getField :: FieldGetter a => (a -> Bool) -> NioForm -> String -> [String] -> Either Int a
+getField validation nf x y = do
+  let nf' = head $ fields nf
+  (\(NioFieldView _ _ _ _ _) -> do
+     let v = (getField' "test")
+     case validation v of
+       True -> Right v
+       False -> Right v
+    )
+    nf'
+
