18886b6276e61d62b9bfb5cfb71b8892c9fd2d30
Chris S <chris@soundshare.co.uk>
2020-07-25 18:10:49 +0100

    Readme and forgotten comit file

---
+++ ghcidTest.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if which nix; then
+    nix-shell --run "ghcid --command='cabal v2-repl test' --test=Test.main"
+    exit 0
+elif which stack; then
+    ghcid '--command=stack ghci niobiumcoconut'
+    exit 0
+fi
